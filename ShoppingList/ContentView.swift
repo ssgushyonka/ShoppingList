8f12d1f87b80e50d16665bf1be04580e630559aa
ssgushyonka <orangeee04@gmail.com>
2025-05-30 16:21:52 +0300

    Initial Commit

---
+++ ShoppingList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShoppingList
+//
+//  Created by Элина Борисова on 30.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShoppingList/ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by Элина Борисова on 30.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
